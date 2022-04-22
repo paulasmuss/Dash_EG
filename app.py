d7e40791de3139ab198282f64797c1003c740b96
paul-first-test.workspace <paul-first-test.workspace>
2022-04-22 12:45:06 +0000

    wip

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app.layout = ddk.App([
                              for x in df['Jahr'].unique().tolist()],
                     value=[],
                     inline=True,
-                    style={"padding": "5px"}
+                    style={"padding": "50px"}
                 )
             ]),
             ddk.Graph(id='update-graph', style={'height':300}),
